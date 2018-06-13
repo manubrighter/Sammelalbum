62604c1d9f42e7e7cd7344f98f968c8860f3457f
aprentas <aprentas@ATB-WSUEK86>
2018-06-13 12:51:39 +0200

    edit Gott

--- src/GUI/PaekchenOeffnen.java
+++ src/GUI/PaekchenOeffnen.java
@@ -4,7 +4,7 @@ public class PaekchenOeffnen {
 
 	public static void main(String[] args) {
 		System.out.println("Test Jimoh!");
-		System.out.println("Hello World:");
+		System.out.println("Gott");
 
 	}
 
