b16bbb4658b821f71bedaccca4cbdeacb51ff009
vmadmin <vmadmin@192.168.1.241>
2018-06-18 22:03:17 +0200

    Erstellung GUI Elemente Startbildschirm

--- src/GUI/Startbildschirm.java
+++ src/GUI/Startbildschirm.java
@@ -1,5 +1,26 @@
 package GUI;
 
-public class Startbildschirm {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Startbildschirm extends JFrame implements ActionListener {
+	
+	//Erstellung der GUI Elemente
+	private JButton albumAnsehen;
+	private JButton paeckchenOeffnen;
+	private JButton neustarten;
+	private JButton programmBeenden;
+	private JLabel fussballbilderSammelalbum;
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		
+	}
 
 }
