80c8742efdd209fbc3ead5776e9c8fc7164fde08
vmadmin <vmadmin@192.168.1.241>
2018-06-18 22:35:08 +0200

    Startbildschirm GUI Elemente beschriftet

--- src/GUI/Startbildschirm.java
+++ src/GUI/Startbildschirm.java
@@ -11,22 +11,31 @@ import javax.swing.JLabel;
 public class Startbildschirm extends JFrame implements ActionListener {
 	
 	//Erstellung der GUI Elemente
-	private JButton albumAnsehen;
-	private JButton paeckchenOeffnen;
-	private JButton neustarten;
-	private JButton programmBeenden;
-	private JLabel fussballbilderSammelalbum;
+	private JButton bAlbumAnsehen;
+	private JButton bPaeckchenOeffnen;
+	private JButton bNeustarten;
+	private JButton bProgrammBeenden;
+	private JLabel lblFussballbilderSammelalbum;
 	
 	//Erstellung Schriften
 	private Font Font50 = new Font("Arial", Font.BOLD, 50);
 	private Font Font40 = new Font("Arial", Font.BOLD, 40);
 	private Font Font20 = new Font("Arial", Font.BOLD, 20);
+	
+	
+	
 
 	
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		
+		//GUI Elemente beschriftet
+		bAlbumAnsehen = new JButton("Album Ansehen");
+		bPaeckchenOeffnen = new JButton ("Päckchen öffnen");
+		bNeustarten = new JButton ("Neustarten");
+		bProgrammBeenden = new JButton ("Beenden");
+		lblFussballbilderSammelalbum = new JLabel ("Fussballbilder Sammelalbum");
 		
 	}
 
