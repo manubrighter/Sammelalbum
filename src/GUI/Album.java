6d0a527a8820ec8a65de67ee9d86d8004c4f06bb
vmuser <vmuser@192.168.2.145>
2018-06-19 16:14:33 +0200

    Album Update

--- src/GUI/Album.java
+++ src/GUI/Album.java
@@ -3,6 +3,7 @@ package GUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,8 +19,8 @@ public class Album extends JFrame implements ActionListener{
 	
 	//Variablen initialisieren
 	private JButton B1;
-	private JLabel LBild1, LBild2, LBild3, LBild4, LBild5, LBild6, LBild7, LBild8, LName1, LName2, LName3, LName4, LStatistik1, LStatistik2, LStatistik3, LStatistik4, LNation1;  
-	private JPanel P1, P2, P3, P4, P5;
+	private JLabel LBild1, LBild2, LBild3, LBild4, LName1, LName2, LName3, LName4, LStatistik1, LStatistik2, LStatistik3, LStatistik4, LNation1;  
+	private JPanel P1, P2, P3, P4, P5, P6;
 
 	private Font Font50 = new Font("Arial", Font.BOLD, 50);
 	private Font Font40 = new Font("Arial", Font.BOLD, 40);
@@ -39,17 +40,22 @@ public class Album extends JFrame implements ActionListener{
 		LBild2 = new JLabel();
 		LBild3 = new JLabel();
 		LBild4 = new JLabel();
-		LBild5 = new JLabel();
-		LBild6 = new JLabel();
-		LBild7 = new JLabel();
-		LBild8 = new JLabel();
 		LNation1= new JLabel("Schweiz");
+		LName1 = new JLabel("Johan Djourou");
+		LName2 = new JLabel("Granit Xhaka");
+		LName3 = new JLabel("Manuel Akanji");
+		LName4 = new JLabel("Haris Seferovic");
+		LStatistik1 = new JLabel("Statistik");
+		LStatistik2 = new JLabel("Statistik");
+		LStatistik3 = new JLabel("Statistik");
+		LStatistik4 = new JLabel("Statistik");
 		
 		P1 = new JPanel();
 		P2 = new JPanel();
 		P3 = new JPanel();
 		P4 = new JPanel();
 		P5 = new JPanel();
+		P6 = new JPanel();
 		
 		P1.setBounds(0, 0, 1150, 650);
 		
@@ -65,16 +71,50 @@ public class Album extends JFrame implements ActionListener{
 		
 		
 		P2.add(LNation1);
+		P2.setBackground(Color.PINK);
 		LNation1.setFont(Font50);
 		
 		
 		P3.setLayout(new BorderLayout());
+		P3.add(P4, BorderLayout.NORTH);
+		P3.add(P5, BorderLayout.CENTER);
+		P3.add(P6, BorderLayout.SOUTH);
 		
+		P4.setBackground(Color.RED);
+		P4.setLayout(new GridLayout(1, 4, 50, 50));
+		P4.add(LName1);
+		P4.add(LName2);
+		P4.add(LName3);
+		P4.add(LName4);
+		LName1.setFont(Font20);
+		LName2.setFont(Font20);
+		LName3.setFont(Font20);
+		LName4.setFont(Font20);
+		P4.setBorder(BorderFactory.createEmptyBorder(20/*top*/, 20/*left*/, 20/*bottom*/, 20/*right*/));
+		
+		
+		P5.setLayout(new GridLayout(1, 4, 10, 10));
+		P5.add(LBild1);
+		P5.add(LBild2);
+		P5.add(LBild3);
+		P5.add(LBild4);	
+		P5.setBorder(BorderFactory.createEmptyBorder(20/*top*/, 20/*left*/, 20/*bottom*/, 20/*right*/));
+		
+		
+		P6.setLayout(new GridLayout(1, 4, 10, 10));
+		P6.add(LStatistik1);
+		P6.add(LStatistik2);
+		P6.add(LStatistik3);
+		P6.add(LStatistik4);
+		LStatistik1.setFont(Font20);
+		LStatistik2.setFont(Font20);
+		LStatistik3.setFont(Font20);
+		LStatistik4.setFont(Font20);
+		P6.setBorder(BorderFactory.createEmptyBorder(20/*top*/, 20/*left*/, 20/*bottom*/, 20/*right*/));
 		
 		
 		this.setVisible(true); 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //das Programm beenden wenn man es schliesst
-		
 	}
 	
 	
