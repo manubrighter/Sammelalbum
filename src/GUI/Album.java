cb2782dcefe075539e9490b8751742faec0008ba
Mustafa <Mustafa@Mohammed>
2018-06-23 20:47:15 +0200

    variablen umbennenung

--- src/GUI/Album.java
+++ src/GUI/Album.java
@@ -19,8 +19,8 @@ public class Album extends JFrame implements ActionListener{
 	
 	
 	//Variablen initialisieren
-	private JButton B1;
-	private JLabel LBild1, LBild2, LBild3, LBild4, LName1, LName2, LName3, LName4, LStatistik1, LStatistik2, LStatistik3, LStatistik4, LNation1;  
+	private JButton bNaechsteSeite, bVorhaerigeSeite, bEinkleben;
+	private JLabel lblBild1, lblBild2, lblBild3, lblBild4, lblName1, lblName2, lblName3, lblName4, lblStatistik1, lblStatistik2, lblStatistik3, lblStatistik4, lblSchweiz;  
 	private JPanel P1, P2, P3, P4, P5, P6;
 
 	private Font Font50 = new Font("Arial", Font.BOLD, 50);
@@ -37,19 +37,19 @@ public class Album extends JFrame implements ActionListener{
 		this.setLocation(530, 350); //Ort des Fensters definieren
 		
 		//Bausteine definieren
-		LBild1 = new JLabel();
-		LBild2 = new JLabel();
-		LBild3 = new JLabel();
-		LBild4 = new JLabel();
-		LNation1= new JLabel("Schweiz");
-		LName1 = new JLabel("Johan Djourou");
-		LName2 = new JLabel("Granit Xhaka");
-		LName3 = new JLabel("Manuel Akanji");
-		LName4 = new JLabel("Haris Seferovic");
-		LStatistik1 = new JLabel("Statistik");
-		LStatistik2 = new JLabel("Statistik");
-		LStatistik3 = new JLabel("Statistik");
-		LStatistik4 = new JLabel("Statistik");
+		lblBild1 = new JLabel();
+		lblBild2 = new JLabel();
+		lblBild3 = new JLabel();
+		lblBild4 = new JLabel();
+		lblSchweiz= new JLabel("Schweiz");
+		lblName1 = new JLabel("Johan Djourou");
+		lblName2 = new JLabel("Granit Xhaka");
+		lblName3 = new JLabel("Manuel Akanji");
+		lblName4 = new JLabel("Haris Seferovic");
+		lblStatistik1 = new JLabel("Statistik");
+		lblStatistik2 = new JLabel("Statistik");
+		lblStatistik3 = new JLabel("Statistik");
+		lblStatistik4 = new JLabel("Statistik");
 		
 		P1 = new JPanel();
 		P2 = new JPanel();
@@ -71,9 +71,9 @@ public class Album extends JFrame implements ActionListener{
 		P1.setBorder(BorderFactory.createEmptyBorder(15/*top*/, 40/*left*/, 100/*bottom*/, 40/*right*/));
 		
 		
-		P2.add(LNation1);
+		P2.add(lblSchweiz);
 		P2.setBackground(Color.PINK);
-		LNation1.setFont(Font50);
+		lblSchweiz.setFont(Font50);
 		
 		
 		P3.setLayout(new BorderLayout());
@@ -83,61 +83,61 @@ public class Album extends JFrame implements ActionListener{
 		
 		P4.setBackground(Color.RED);
 		P4.setLayout(new GridLayout(1, 4, 50, 50));
-		P4.add(LName1);
-		P4.add(LName2);
-		P4.add(LName3);
-		P4.add(LName4);
-		LName1.setFont(Font20);
-		LName2.setFont(Font20);
-		LName3.setFont(Font20);
-		LName4.setFont(Font20);
+		P4.add(lblName1);
+		P4.add(lblName2);
+		P4.add(lblName3);
+		P4.add(lblName4);
+		lblName1.setFont(Font20);
+		lblName2.setFont(Font20);
+		lblName3.setFont(Font20);
+		lblName4.setFont(Font20);
 		P4.setBorder(BorderFactory.createEmptyBorder(20/*top*/, 20/*left*/, 20/*bottom*/, 20/*right*/));
-		LName1.setHorizontalAlignment(SwingConstants.CENTER);
-		LName1.setVerticalAlignment(SwingConstants.CENTER);
-		LName2.setHorizontalAlignment(SwingConstants.CENTER);
-		LName2.setVerticalAlignment(SwingConstants.CENTER);
-		LName3.setHorizontalAlignment(SwingConstants.CENTER);
-		LName3.setVerticalAlignment(SwingConstants.CENTER);
-		LName4.setHorizontalAlignment(SwingConstants.CENTER);
-		LName4.setVerticalAlignment(SwingConstants.CENTER);
+		lblName1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName1.setVerticalAlignment(SwingConstants.CENTER);
+		lblName2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName2.setVerticalAlignment(SwingConstants.CENTER);
+		lblName3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName3.setVerticalAlignment(SwingConstants.CENTER);
+		lblName4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName4.setVerticalAlignment(SwingConstants.CENTER);
 		
 		
 		P5.setLayout(new GridLayout(1, 4, 10, 10));
-		P5.add(LBild1);
-		P5.add(LBild2);
-		P5.add(LBild3);
-		P5.add(LBild4);	
+		P5.add(lblBild1);
+		P5.add(lblBild2);
+		P5.add(lblBild3);
+		P5.add(lblBild4);	
 		P5.setBorder(BorderFactory.createEmptyBorder(20/*top*/, 20/*left*/, 20/*bottom*/, 20/*right*/));
 		P5.setBackground(Color.BLUE);
-		LBild1.setHorizontalAlignment(SwingConstants.CENTER);
-		LBild1.setVerticalAlignment(SwingConstants.CENTER);
-		LBild2.setHorizontalAlignment(SwingConstants.CENTER);
-		LBild2.setVerticalAlignment(SwingConstants.CENTER);
-		LBild3.setHorizontalAlignment(SwingConstants.CENTER);
-		LBild3.setVerticalAlignment(SwingConstants.CENTER);
-		LBild4.setHorizontalAlignment(SwingConstants.CENTER);
-		LBild4.setVerticalAlignment(SwingConstants.CENTER);
+		lblBild1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBild1.setVerticalAlignment(SwingConstants.CENTER);
+		lblBild2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBild2.setVerticalAlignment(SwingConstants.CENTER);
+		lblBild3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBild3.setVerticalAlignment(SwingConstants.CENTER);
+		lblBild4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBild4.setVerticalAlignment(SwingConstants.CENTER);
 		
 		
 		P6.setLayout(new GridLayout(1, 4, 10, 10));
-		P6.add(LStatistik1);
-		P6.add(LStatistik2);
-		P6.add(LStatistik3);
-		P6.add(LStatistik4);
-		LStatistik1.setFont(Font20);
-		LStatistik2.setFont(Font20);
-		LStatistik3.setFont(Font20);
-		LStatistik4.setFont(Font20);
+		P6.add(lblStatistik1);
+		P6.add(lblStatistik2);
+		P6.add(lblStatistik3);
+		P6.add(lblStatistik4);
+		lblStatistik1.setFont(Font20);
+		lblStatistik2.setFont(Font20);
+		lblStatistik3.setFont(Font20);
+		lblStatistik4.setFont(Font20);
 		P6.setBorder(BorderFactory.createEmptyBorder(20/*top*/, 20/*left*/, 20/*bottom*/, 20/*right*/));
 		P6.setBackground(Color.GRAY);
-		LStatistik1.setHorizontalAlignment(SwingConstants.CENTER);
-		LStatistik1.setVerticalAlignment(SwingConstants.CENTER);
-		LStatistik2.setHorizontalAlignment(SwingConstants.CENTER);
-		LStatistik2.setVerticalAlignment(SwingConstants.CENTER);
-		LStatistik3.setHorizontalAlignment(SwingConstants.CENTER);
-		LStatistik3.setVerticalAlignment(SwingConstants.CENTER);
-		LStatistik4.setHorizontalAlignment(SwingConstants.CENTER);
-		LStatistik4.setVerticalAlignment(SwingConstants.CENTER);
+		lblStatistik1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatistik1.setVerticalAlignment(SwingConstants.CENTER);
+		lblStatistik2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatistik2.setVerticalAlignment(SwingConstants.CENTER);
+		lblStatistik3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatistik3.setVerticalAlignment(SwingConstants.CENTER);
+		lblStatistik4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatistik4.setVerticalAlignment(SwingConstants.CENTER);
 		
 		
 		this.setVisible(true); 
