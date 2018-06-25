f705dec6c00ffb54624b23f7a2d986ebd1b8356d
Mustafa <Mustafa@Mohammed>
2018-06-25 23:46:21 +0200

    Junit comments

--- src/JUnit_Test/JUnit_Tests.java
+++ src/JUnit_Test/JUnit_Tests.java
@@ -15,16 +15,28 @@ class JUnit_Tests {
 	private static HauptLogik hl;
 	private static Unparteiische up;
 
+	/**
+	 * Wird vor dem Testen ausgeführt
+	 * ein Objekt zur Klasse HauptLogik und Unparteiische erstellen
+	 * @throws Exception
+	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		hl = new HauptLogik();		
 		up = new Unparteiische();	
 	}
 
+	/**
+	 * Wird vor jedem Test ausgeführt
+	 * @throws Exception
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
+	/**
+	 * testet ob man die richtigen Werte bekommt wenn man eine Abfrage der MenschenListe macht
+	 */
 	@Test
 	void testgetMenschenListe() {
 		
@@ -35,6 +47,9 @@ class JUnit_Tests {
 		assertTrue(test1.contains("15"));
 	}
 	
+	/**
+	 * testet ob der Konstruktor der für das erstellen eines Unparteiischen zuständig ist richtig funktioniert
+	 */
 	@Test
 	void testcreateUnparteiische() {
 		
