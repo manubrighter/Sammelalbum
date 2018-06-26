00563c7c0e0c074c28f33af523b10cfa37ecac30
vmuser <vmuser@192.168.2.139>
2018-06-26 13:36:06 +0200

    comments

--- src/Daten/Unparteiische.java
+++ src/Daten/Unparteiische.java
@@ -1,14 +1,26 @@
 package Daten;
 
+/**
+ * Daten der Unparteiischen
+ * @author Jimoh Meyer & Manuel Heller
+ *
+ */
 public class Unparteiische extends Mensch{
 	
 	private int AnzPfiffe;
 	
+	/**
+	 * erstellt ein Unparteiischen
+	 */
 	public void createUnparteiische(int Alter, int AnzPfiffe) {
 		super.createUnparteiische(Alter, AnzPfiffe);
 		this.AnzPfiffe = AnzPfiffe;
 	}
 
+	/**
+	 * Erstellt eine String aus den lokalen Variablen
+	 * @return
+	 */
 	public String UnparteiischeString() {
 		StringBuilder sbTrainer = new StringBuilder();
 
